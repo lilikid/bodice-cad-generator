6a31650f1ce7b9af4893a1e64e2be42431804d87
lilikid <lilinorakids@gmail.com>
2026-09-12 14:18:36 +0400

    Add drafting rectangle

--- back-bodice.js
+++ back-bodice.js
@@ -1,1 +1,48 @@
+class BackBodice {
 
+    constructor(draftingEngine) {
+
+        this.draftingEngine =
+            draftingEngine;
+
+    }
+
+    drawDraftingRectangle() {
+
+        const left = 300;
+        const top = 150;
+
+        const width = 220;
+        const height = 420;
+
+        this.draftingEngine.drawLine(
+            left,
+            top,
+            left + width,
+            top
+        );
+
+        this.draftingEngine.drawLine(
+            left + width,
+            top,
+            left + width,
+            top + height
+        );
+
+        this.draftingEngine.drawLine(
+            left + width,
+            top + height,
+            left,
+            top + height
+        );
+
+        this.draftingEngine.drawLine(
+            left,
+            top + height,
+            left,
+            top
+        );
+
+    }
+
+}
